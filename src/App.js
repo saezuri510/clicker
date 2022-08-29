67af88cae2dd79f5bcf279ebe5b43530d6618e21
saezuri510 <saezuru510@gmail.com>
2022-08-29 14:16:13 +0900

    仮完了

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [count, setCount] = useState(0);
+    return (
+        <>
+            <p>You Clicked {count} times</p>
+            <button onClick={() => setCount((prev) => prev + 1)}>+1</button>
+            <button onClick={() => setCount((prev) => prev - 1)}>-1</button>
+            <button onClick={() => setCount(0)}>Reset</button>
+            <button onClick={() => setCount((prev) => prev + 2)}>
+                Double Add
+            </button>
+        </>
+    );
 }
 
 export default App;
