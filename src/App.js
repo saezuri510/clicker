0aacf88aca5ec08541de52764bb5d321585bc1ce
saezuri510 <saezuru510@gmail.com>
2022-08-30 10:26:10 +0900

    完了

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import { useState } from "react";
 
 function App() {
+    //useStateでsetCount関数が変化したときcountが変わる
     const [count, setCount] = useState(0);
+    const add = () => setCount((prevCount) => prevCount + 1);
+    const doubleAdd = () => {
+        add();
+        add();
+    };
     return (
         <>
             <p>You Clicked {count} times</p>
-            <button onClick={() => setCount((prev) => prev + 1)}>+1</button>
+            <button onClick={add}>+1</button>
             <button onClick={() => setCount((prev) => prev - 1)}>-1</button>
             <button onClick={() => setCount(0)}>Reset</button>
-            <button onClick={() => setCount((prev) => prev + 2)}>
-                Double Add
-            </button>
+            <button onClick={doubleAdd}>Double Add</button>
         </>
     );
 }
