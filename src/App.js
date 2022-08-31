85d7253d6b1b41e9a9d95fe285b3b78f0fa8e1bb
saezuri510 <saezuru510@gmail.com>
2022-08-31 20:01:28 +0900

    map途中3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 const WEATHER = [
-    { weather: sunny, umbrella: false },
-    { weather: cloudy, umbrella: false },
-    { weather: rainy, umbrella: true },
-    { weather: snowy, umbrella: true },
-    { weather: windy, umbrella: false },
+    { weather: "sunny", umbrella: false },
+    { weather: "cloudy", umbrella: false },
+    { weather: "rainy", umbrella: true },
+    { weather: "snowy", umbrella: true },
+    { weather: "windy", umbrella: false },
 ];
 
-function App(){
-    return()
+function App() {
+    return (
+        <ul>
+            {WEATHER.map((value) => {
+                return <li>{value.weather}</li>;
+            })}
+        </ul>
+    );
 }
 
 export default App;
