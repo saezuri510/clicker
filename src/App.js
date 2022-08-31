8925cbe7cb239baad113b679211842c3b7d5a7a4
saezuri510 <saezuru510@gmail.com>
2022-08-31 18:31:17 +0900

    map途中

---
+++ src/App.js
@@ -0,0 +1,7 @@
+const WEATHER = [
+    { weather: sunny, umbrella: false },
+    { weather: cloudy, umbrella: false },
+    { weather: rainy, umbrella: true },
+    { weather: snowy, umbrella: true },
+    { weather: windy, umbrella: false },
+];
